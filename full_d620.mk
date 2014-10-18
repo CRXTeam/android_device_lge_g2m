c7ee91b30888388e157378a018035770b71dbce2
maxprzemo <przemekbanasiak1@wp.pl>
2014-10-18 12:11:16 +0200

    Update full_d620.mk

--- full_d620.mk
+++ full_d620.mk
@@ -16,7 +16,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, device/lge/d620/device_d620.mk)
+$(call inherit-product, device/lge/d620/d620.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d620
