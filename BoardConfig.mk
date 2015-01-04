52e433d180dc8d00af148255d2eef59aa770c39a
maxprzemo <przemekbanasiak1@wp.pl>
2015-01-04 12:31:17 +0100

    deleted SElinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,10 +122,10 @@ BOARD_HARDWARE_CLASS := device/lge/d620/cmhw/
 
 # SELinux policies
 # qcom sepolicy
-include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += \
-        device/lge/d620/sepolicy
+#BOARD_SEPOLICY_DIRS += \
+#        device/lge/d620/sepolicy
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/d620/bluetooth
